ac61710f651149be670b866d08efcf3445f5fac1
sabmoh1 <sabersisox0554@gmail.com>
2025-07-17 14:37:04 +0100

    Update app.py

--- app.py
+++ app.py
@@ -108,8 +108,8 @@ def outfit_image():
     positions[6]['y'] -= 80
 
     # رفع وتحريك المربع النهائي
-    positions[3]['x'] += -30
-    positions[3]['y'] -= 10
+    positions[3]['x'] += -20
+    positions[3]['y'] += 20
 
     for idx, future in enumerate(outfit_images):
         outfit_image = future.result()
