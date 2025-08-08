9c7cbf38b09dd3e7bdd2699cd1fc42731e3ae4d2
sabmoh1 <sabersisox0554@gmail.com>
2025-08-08 12:20:34 +0100

    Update app.py

--- app.py
+++ app.py
@@ -160,4 +160,4 @@ def outfit_image():
     return send_file(img_io, mimetype='image/png')
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)    key = request.args.get('key')
+    app.run(debug=True, host='0.0.0.0', port=5000)
